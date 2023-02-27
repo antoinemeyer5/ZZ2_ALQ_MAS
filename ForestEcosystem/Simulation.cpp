2de64fbb6101163ac50371171ee8f023ad226642
antoinemeyer5 <antoine.meyer54@gmail.com>
2023-02-27 14:35:30 +0100

    List of actors

--- ForestEcosystem/Simulation.cpp
+++ ForestEcosystem/Simulation.cpp
@@ -6,23 +6,28 @@
 
 int main(int, char**) {
     Environment environment(100, 10, 0, 0);
-    Lumberjack a;
-    Carpenter b;
+
+    //Agent* actors[] = { new Lumberjack(), new Carpenter() }; 
+    // => 11 round give : 80 trees, 10 woods and 50 planks
+    
+    Agent* actors[] = { new Lumberjack(), new Carpenter(), new Lumberjack(), new Carpenter() };
+    // => 11 round give : 20 trees, 10 woods and 80 planks
+
 
     // TODO
-    // - faire une liste d'agents (bucheron, menuisier, etc)
     // - tirer au hasard l'ordre dans lequel il travaille (ajout d'aleatoire)
+    // - tracer des graphes avec les évolutions des courbes suivant la configuration de départ
 
     // tour de simulation
     for(int i=1; i<12; i++) {
-        std::cout << "\033[4mStart of the round n°" << i << ":\033[0m\n";
+        std::cout << "\033[4mStart of the round n°" << i << ":\033[0m\n\n";
         std::cout << environment.BarDisplay();
-        // bucheron change son statut puis fait quelque chose
-        std::cout << a.toString();
-        a.DoSomething(environment);
-        // menuisier change son status puis fait quelque chose 
-        std::cout << b.toString();
-        b.DoSomething(environment);
+        // pour chaque acteurs
+        for(int i=0; i<4; i++){
+            // bucheron change son statut puis fait quelque chose
+            std::cout << actors[i]->toString();
+            actors[i]->DoSomething(environment);
+        }
         // environnement se regenere un peu
         environment.Increase(10, trees);
         std::cout << "==> Environment increase \033[42m10 trees\033[0m\n";
